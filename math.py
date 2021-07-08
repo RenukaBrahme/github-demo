928c3c97313bce808c933a22d93b642b240b2737
RenukaBrahme <83638968+RenukaBrahme@users.noreply.github.com>
2021-07-08 14:28:21 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# Add Implementation
+def add(x,y):
+	return x+y;
+# Subtract Implementation	
+def subtract(x,y):
+	return x-y;
+# Multiply Implementation	
+def multiply(x,y):
+	return x*y;
+# Divide Implementation	
+def divide(x,y):
+	if y==0:
+		return DIVIDE_BY_0_ERROR
+	else:
+		return x/y;
+
+#Square Implementation		
+def square(x,y):
+	pass
